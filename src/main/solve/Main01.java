172e14457ff1d2ffc53f3db860616d2f091eb2fb
kmj030605 <kmj030605@nsmall.com>
2025-08-06 17:37:30 +0900

    study1

--- src/main/solve/Main01.java
+++ src/main/solve/Main01.java
@@ -1,9 +1,59 @@
 package main.solve;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main01 {
 
     public static void main(String[] args) {
+        int even=0;
+        int odd=0;
+        Scanner sc = new Scanner(System.in); //scanner로 예제를 입력받음.
+        List<String> peopleList= new ArrayList<>(); //사원 저장해놓는 리스트
 
-        System.out.println("Hello World");
+        //사원 수 입력
+        System.out.print("사원 수 입력: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+
+        for (int i = 1; i <= num; i++) {
+            System.out.print("사원 정보를(사원이름/사번/입사일(yyyymmdd)) 형태로 입력하세요: ");
+            String people = sc.nextLine();
+
+            peopleList.add(people);
+
+        }
+
+        for(String people:peopleList){
+            String[] peopleSplit = people.split("/");
+
+            String pname = peopleSplit[0];
+            int pnum = Integer.parseInt(peopleSplit[1]);
+            int pdate = Integer.parseInt(peopleSplit[2]);
+
+
+
+            if(pdate>20240701){
+                System.out.println("-------------------------------");
+                if (pnum % 2 == 0){
+                    System.out.println(pname+" / "+pnum+" / "+"even");
+                    even++;
+                }
+                else  {
+                    System.out.println(pname+" / "+pnum+" / "+"odd");
+                    odd++;
+                }
+
+            }
+
+
+        }
+
+        System.out.println("odd : "+odd);
+        System.out.println("even : "+even);
+        int sum=odd+even;
+        System.out.println("sum : "+sum);
     }
 }
